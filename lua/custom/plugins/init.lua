c541e2ff9d0af4b269bbeac51bafa5864904cbaa
Brian Kariu <waiganjo.brian@gmail.com>
2023-11-08 20:59:39 +0300

    feat: Added black formatter

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,5 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+ local M = {}
+ M.ui = {theme = 'catppuccin'}
+ M.plugins = "custom.plugins"
+ M.mappings = require "custom.plugins.mappings"
+ return M
